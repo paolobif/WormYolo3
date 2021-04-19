e09b31759407b53cf43ab08e12426ba22a1e786c
JakLemur <44098796+JakLemur@users.noreply.github.com>
2021-04-19 10:35:52 -0700

    BB_TOD_calls
    SORT time of death update and several bulk scripts

---
+++ ProcessSortAD.py
@@ -0,0 +1,143 @@
+import os
+import sys
+import cv2
+import csv
+import tqdm
+import pandas as pd
+import random
+import numpy as np
+import time
+from skimage.morphology import skeletonize, medial_axis
+from matplotlib import pyplot as plt
+from re import sub
+
+
+def bb_intersection_over_union(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
+
+
+
+def analyzeSORT(df,threshold):
+    threshold = 45
+    vc = df.label.value_counts()
+    test = vc[vc > threshold].index.tolist()
+    csv_outputs = []
+    deadboxes = []
+    for ID in test:        
+        filtval = df['label'] ==ID
+        interim = df[filtval]
+        interimD = []
+        interim2 = np.asarray(interim)
+        fill = 0
+        deadcount = 0
+        for row in interim2:
+            frameNA, x1A, y1A, x2A, y2A, labelA, deltaA, catagoryA, *_ = row
+            if fill > 1:
+                boxA = [x1A, y1A, x2A, y2A]
+                boxB = [x1B, y1B, x2B, y2B]
+                deltaA = bb_intersection_over_union(boxA, boxB) 
+                #print(deltaA)
+                if deltaA > 0.95:
+                    deadcount += 1
+                    #print(deadcount)
+                #print(fill)
+                if deadcount > 10:
+                    catagoryA = 'dead'
+                   # print(deadcount)
+                if deadcount == 11:
+                    if deadboxes == []:
+                        deadboxes.append([x1A, y1A, x2A, y2A])               
+                        csv_outputs.append(frameNA)
+                    else:
+                        isunique = 0
+                        for box in deadboxes:
+                            #print(box)
+                            x1D, y1D, x2D, y2D, *_ = box
+                            boxD = [x1D, y1D, x2D, y2D]
+                            deltaD = bb_intersection_over_union(boxA, boxD)  
+                            if deltaD > 0.8:
+                                isunique = 1
+                        if isunique == 0:
+                            deadboxes.append([x1A, y1A, x2A, y2A])               
+                            csv_outputs.append(int(frameNA))
+
+                    #print(deathtime)
+                    #print(frameNA)
+                #newRow = [frameNA, x1A, y1A, x2A, y2A, labelA, deltaA, catagoryA]
+                #interimD.append(newRow)
+            frameNB, x1B, y1B, x2B, y2B,labelB, deltaB, catagoryB, *_ = row
+            fill +=1
+            #interimD.append(deathtime)
+        #interimD = pd.DataFrame(interimD, columns = ['deathframe'])
+        #csv_outputs.append(interimD)
+    #large_df = pd.concat(csv_outputs, ignore_index=True)  
+    csv_outputs = pd.DataFrame(csv_outputs, columns = ['#desc'])
+    csv_outputs['neural'] = '1'
+    return(csv_outputs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    # declare source directory and out path
+    """
+    IMG_DIR is path to the folder with the images
+    OUT_PATH is the path to the csv file you would like the output to go to
+    i.e './output/sample.csv'
+    """
+    SORT_DIR = sys.argv[1]
+    WORMLIST_DIR = sys.argv[2]
+    OUT_PATH = sys.argv[3]
+    
+    
+    csv_list = os.listdir(SORT_DIR)
+    worm_list = os.listdir(WORMLIST_DIR)
+    for csv_name in csv_list:
+        expname = sub("_sort_forward.csv","",csv_name)
+        wormlist_name ="wormlist_" + expname + ".csv"
+        
+        csv_path = os.path.join(SORT_DIR, csv_name)
+        df = pd.read_csv(csv_path,names=('frame', 'x1', 'y1', 'x2', 'y2','label','delta'))
+        df['catagory'] = 'alive'
+
+        outputs = analyzeSORT(df,threshold = 45)
+        out_name = expname + "_AD.csv"
+        out_csv_path = os.path.join(OUT_PATH, out_name)
+        pd.DataFrame(outputs).to_csv(out_csv_path, mode='w', header=True, index=None)
+        
+
+        csv_path = os.path.join(WORMLIST_DIR, wormlist_name)
+        wldf = pd.read_csv(csv_path,names=('x1','y1','#desc', 'label','deathday','unix')) 
+        wldf = wldf[['#desc']]
+        wldf['manual'] = '1'
+        pd.DataFrame(wldf).to_csv(out_csv_path, mode='a', header=True, index=None)
+
+        
